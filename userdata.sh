826537ef1c7dfabf80d0f1ef519b3a5d9c07cd00
nick1846 <73354114+nick1846@users.noreply.github.com>
2021-01-05 21:37:29 -0600

    Update userdata.sh

--- userdata.sh
+++ userdata.sh
@@ -3,3 +3,9 @@ sudo yum update -y
 sudo amazon-linux-extras install epel -y
 sudo yum install git -y
 sudo amazon-linux-extras install docker
+sudo wget -O /etc/yum.repos.d/jenkins.repo https://pkg.jenkins.io/redhat-stable/jenkins.repo
+sudo rpm --import https://pkg.jenkins.io/redhat-stable/jenkins.io.key
+sudo yum upgrade -y
+sudo yum install jenkins java-1.8.0-openjdk-devel -y
+sudo systemctl daemon-reload
+sudo systemctl start jenkins
