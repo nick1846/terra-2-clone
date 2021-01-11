5d130c0203f0e94e862a78de9746e5f961285509
nick1846 <73354114+nick1846@users.noreply.github.com>
2021-01-11 10:07:24 -0600

    Update userdata.sh

--- userdata.sh
+++ userdata.sh
@@ -3,6 +3,11 @@ sudo yum update -y
 sudo amazon-linux-extras install epel -y
 sudo yum install git -y
 sudo amazon-linux-extras install docker
+sudo service docker start
+sudo service docker enable
+sudo usermod -a -G docker ec2-user
+sudo curl -L "https://github.com/docker/compose/releases/download/1.27.4/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+sudo chmod +x /usr/local/bin/docker-compose
 sudo wget -O /etc/yum.repos.d/jenkins.repo https://pkg.jenkins.io/redhat-stable/jenkins.repo
 sudo rpm --import https://pkg.jenkins.io/redhat-stable/jenkins.io.key
 sudo yum upgrade -y
